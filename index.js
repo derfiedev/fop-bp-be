e35c1cf6a18fad914adb73ee953b69d5cceeb282
derfie <89699885+derfiedev@users.noreply.github.com>
2025-05-30 10:24:19 +0200

    added frontend

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+
+app.use(express.static('dist'))
 app.use(cors())
 
 app.use(express.json())
